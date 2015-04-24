a8d91ba898f4aadf68d7779db359e65a6a8c4d9e
derrikengel <derrik@derrikengel.com>
2015-04-24 15:40:28 -0500

    homepage media queries

--- front-end/scripts/scripts.js
+++ front-end/scripts/scripts.js
@@ -5,6 +5,7 @@ $(function () {
 	   ========================================================================== */
 	var navContainer = $('.site-nav');
 	var mainNav = $('.main-nav');
+	var slideContainer = $('.carousel');
 
 
 	/* ==========================================================================
@@ -62,6 +63,67 @@ $(function () {
 			$('.date-from').datepicker('option', 'maxDate', selectedDate);
 		}
 	});
+
+
+	/* ==========================================================================
+	   Carousel
+	   ========================================================================== */
+	if (slideContainer.length) {
+		var slideInterval;
+		var slideshowPosition = 0;
+		var totalSlides = slideContainer.find('.slide').length;
+		var slideSpeed = slideContainer.data('speed') || 6000;
+
+		slideContainer.append('<ul class="slide-nav"></ul>');
+		for (i = 1; i <= totalSlides; i++) {
+			slideContainer.find('.slide-nav').append('<li' + (i == 1 ? " class=\"active\"" : "") + ' title="' + i + ' of ' + totalSlides + '">' + i + '</li>');
+		}
+
+		slideContainer.find('.next').click(function () {
+			clearInterval(slideInterval);
+			nextSlide();
+		});
+
+		slideContainer.find('.prev').click(function () {
+			clearInterval(slideInterval);
+			prevSlide();
+		});
+
+		function nextSlide() {
+			if (slideshowPosition >= totalSlides - 1) {
+				slideshowPosition = 0;
+			} else {
+				slideshowPosition++;
+			}
+			newSlide();
+		}
+
+		function prevSlide() {
+			if (slideshowPosition == 0) {
+				slideshowPosition = totalSlides - 1;
+			} else {
+				slideshowPosition--;
+			}
+			newSlide();
+		}
+
+		function newSlide() {
+			slideContainer.find('.slide:eq(' + slideshowPosition + ')').addClass('animating').fadeIn(function () {
+				/* hide all images except active slide */
+				slideContainer.find('.slide').not(':eq(' + slideshowPosition + ')').hide();
+
+				/* remove animating class from active slide and show it
+				 (use display: block instead of show because images will default to inline) */
+				slideContainer.find('.slide:eq(' + slideshowPosition + ')').css('display', 'block').removeClass('animating');
+
+				/* update the slideshow pagination */
+				slideContainer.find('.slide-nav li').removeClass('active');
+				slideContainer.find('li:eq(' + slideshowPosition + ')').addClass('active');
+			});
+		};
+
+		slideInterval = setInterval(nextSlide, slideSpeed);
+	}
 
 
 });
